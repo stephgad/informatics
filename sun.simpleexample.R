8f9e39ac93002986f0aa6104dff9ac7960dad76c
Stephanie Gad <stephanie.gad7@gmail.com>
2017-04-24 18:43:52 -0700

    reset back to orange

--- sun.simpleexample.R
+++ sun.simpleexample.R
@@ -3,5 +3,9 @@ sunlight = read.table("sun.txt", header=T)
 par(mar=c(5,6,3,2))
 boxplot(sunlight$Kdown_direct~sunlight$month,
   ylab="Downwelling Solar\n kj/m2/day",
-  xlab="Month", col="orange")
+<<<<<<< HEAD
+  xlab="Month", col="pink")
+=======
+  xlab="month", col="orange")
+>>>>>>> parent of 7381083... Capitalized Month
 
